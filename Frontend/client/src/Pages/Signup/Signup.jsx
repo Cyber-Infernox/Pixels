9eda7af70eb3fc2d120374e586c5cd05825d78c7
Cyber_Infernox <sayonkar@gmail.com>
2023-07-10 23:17:22 +0530

    Update Signup.jsx

--- Frontend/client/src/Pages/Signup/Signup.jsx
+++ Frontend/client/src/Pages/Signup/Signup.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useSignup } from "../../Hooks/useSignup";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -11,13 +12,20 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    await signup(email, password);
+    await signup(username, email, password);
   };
 
   return (
     <div className="h-screen py-[150px] px-[500px]">
       <form className="flex flex-col h-full justify-center items-center">
         <h3>Signup</h3>
+
+        <label>Username:</label>
+        <input
+          type="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
         <label>Email:</label>
         <input
